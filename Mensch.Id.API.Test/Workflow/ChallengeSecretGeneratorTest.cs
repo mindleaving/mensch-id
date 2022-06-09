8da2229c6e8a918a34a3aeafc9722d938c475d9f
Jan Scholtyssek <jan@janscholtyssek.dk>
2022-06-09 20:34:26 +0200

    Added ChallengeSecretGeneratorTest

---
+++ Mensch.Id.API.Test/Workflow/ChallengeSecretGeneratorTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Mensch.Id.API.Workflow;
+using NUnit.Framework;
+
+namespace Mensch.Id.API.Test.Workflow
+{
+    internal class ChallengeSecretGeneratorTest
+    {
+        [Test]
+        public void NonPositiveChallengeLengthThrowsException()
+        {
+            Assert.That(() => ChallengeSecretGenerator.Generate(-1), Throws.TypeOf<ArgumentOutOfRangeException>());
+            Assert.That(() => ChallengeSecretGenerator.Generate(0), Throws.TypeOf<ArgumentOutOfRangeException>());
+        }
+
+        [Test]
+        public void ChallengeSecretHasExpectedLength()
+        {
+            const int Length = 10;
+            var actual = ChallengeSecretGenerator.Generate(Length);
+
+            Assert.That(actual, Has.Length.EqualTo(Length));
+        }
+
+        [Test]
+        public void ChallengeSecretNotEmpty()
+        {
+            var actual = ChallengeSecretGenerator.Generate();
+
+            Assert.That(actual, Is.Not.Null);
+            Assert.That(actual, Is.Not.Empty);
+        }
+    }
+}
--- Mensch.Id.API/Workflow/ChallengeSecretGenerator.cs
+++ Mensch.Id.API/Workflow/ChallengeSecretGenerator.cs
@@ -1,9 +1,13 @@
-﻿namespace Mensch.Id.API.Workflow
+﻿using System;
+
+namespace Mensch.Id.API.Workflow
 {
     public class ChallengeSecretGenerator
     {
         public static string Generate(int challengeLength = 6)
         {
+            if(challengeLength <= 0)
+                throw new ArgumentOutOfRangeException(nameof(challengeLength));
             var passwordGenerator = new TemporaryPasswordGenerator { AllowedCharacters = "ABCDEFGHJKLMNPQRSTUVWXYZ23456789" };
             return passwordGenerator.Generate(challengeLength).ToUpper();
         }
