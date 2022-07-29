dcc31a2aacd8ce5d7b8995afca4ecedfaf36ace5
Jan Scholtyssek <jan@janscholtyssek.dk>
2022-07-29 22:02:57 +0200

    Added method for obscuring existing external ID

---
+++ Mensch.Id.API.Tools/ExternalLoginTools.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Mensch.Id.API.AccessControl;
+using NUnit.Framework;
+
+namespace Mensch.Id.API.Tools
+{
+    internal class ExternalLoginTools
+    {
+        [Test]
+        [TestCase("")]
+        public void ObscureExternalId(string externalId)
+        {
+            const string Salt = "";
+            const string Secret = "";
+            var obscurer = new ExternalLoginObscurer(Salt, Secret);
+            var obscuredExternalId = obscurer.Obscure(externalId);
+            Console.WriteLine(obscuredExternalId);
+        }
+    }
+}
