e81e691702730800c158bd934475e9d36c046a29
Jan Scholtyssek <jan@janscholtyssek.dk>
2023-10-10 01:46:18 +0200

    Added extended terms and conditions for German.

---
+++ mensch-id-frontend/src/localComponents/components/TermsAndConditions/TermsAndConditions_DE.tsx
@@ -0,0 +1,120 @@
+interface TermsAndConditions_DEProps {}
+
+export const TermsAndConditionsDE = (props: TermsAndConditions_DEProps) => {
+
+    return (
+    <>
+        <h1>Allgemeine Geschäftsbedingungen</h1>
+        <p>
+            <small>Stand der AGB Oktober 2023</small>.
+        </p>
+        <h3>Definitionen</h3>
+        <p>
+            <strong>mensch.ID</strong> Ist ein Identifikationswort bestehend aus acht Zahlen, einem Bindestrich und fünf Buchstaben und Zahlen. Eine mensch.ID wird von dem Server mit der Internetadresse "mensch.id" vergeben und ist für jeden Benutzer einzigartig.
+        </p>
+        <p>
+            <strong>Regulärer Benutzer</strong> Ein regulärer Benutzer verwendet die Internetseite https://mensch.id/ zu dem Zweck eine einzelne mensch.ID zu erhalten und diese zu verwalten.
+        </p>
+        <p>
+            <strong>Vergabekonto</strong> Ist ein Nutzerkonto das es erlaubt mehrere mensch.IDs für andere Personen zu erstellen.
+        </p>
+        <p>
+            <strong>Person</strong> Ein Lebewesen das der Art Mensch (homo sapiens) angehört.
+        </p>
+        <p>
+            <strong>Nutzer</strong> Jede natürliche oder juristische Person oder Personengruppe mit einem Konto auf dieser Internetseite.
+        </p>
+        <p>
+            <strong>Betreiber</strong> Die im Impressum dieser Internetseite angegebene Person oder Personengruppe.
+        </p>
+        <h3>Reguläre Benutzer</h3>
+        <p>
+            Ein reguläres Benutzerkonto kann erstellt werden durch
+        </p>
+        <ol>
+            <li>die Nutzung der "Registrieren"-Schaltfläche und dem Ausfüllen des zugehörigen Registrierungsformulars <br />oder</li>
+            <li>die Schaltflächen mit Symbolen von externen Anmeldeanbietern (Google, X (ehemals Twitter), Windows oder Facebook)</li>
+        </ol>
+        <p>
+            auf der Anmeldeseite. Durch das Erstellen eines neuen Benutzerkontos bestätigst du, dass...
+        </p>
+        <ul>
+            <li>...dir, oder die Person für die du das Konto erstellst, bisher keine mensch.ID zugeordnet ist oder dass das Konto erstellt wird mit dem Ziel eine von einer Vergabestelle erstellte mensch.ID zu übernehmen</li>
+            <li>...du die mensch.ID für dich selber oder einen anderen Menschen nutzt. Insbesondere ist es nicht erlaubt eine mensch.ID für Tiere oder Gegenstände zu erstellen</li>
+            <li>...du, wenn du das Konto für eine andere Person erstellst, aktuell die Vorsorgevollmacht für diese Person hast</li>
+        </ul>
+        <p>
+            Grundsätzlich ist jeder Mensch nur berechtigt eine (1) mensch.ID zu bekommen. Ausnahmen hiervon müssen vom Betreiber von mensch.ID explizit und schriftlich genehmigt werden.
+        </p>
+        <p>
+            Bei Verdacht dass ein Nutzer gegen Bestimmungen in dieser AGB verstößt, können alle mit diesem Nutzer verknüpften mensch.IDs zurückgezogen und freigegeben werden, um in Zukunft mit neuen Benutzern verknüpft zu werden oder durch eine Vergabestelle neu vergeben zu werden. Alle Nutzerkonten, die mit diesen mensch.IDs verknüpft waren, werden gelöscht.
+        </p>
+        <h3>Bestimmungen für Vergabekonten</h3>
+        <p>
+            Durch Nutzung des Vergabekontos sicherst du zu...
+        </p>
+        <ul>
+            <li>...nur mensch.IDs zu erstellen für Personen die bisher keine mensch.ID mit sich verknüpft haben. Hierfür wird mindestens mündlich nach der Existenz einer dieser Person zugeordneten mensch.ID gefragt.</li>
+            <li>...nach der Erstellung einer mensch.ID der Person oder einem Vorsorgebevollmächtigten das Besitzzertifikat auszustellen, entweder in Papierform oder elektronisch.</li>
+        </ul>
+        <h3>Nutzung des Webshops</h3>
+        <p>
+            <small>Anmerkung: Der Webshop ist aktuell nur Nutzern mit Vergabekonten zugänglich</small>
+        </p>
+        <p>
+            Der Webshop ist bei Vergabekonten über das Menü "Funktionen" aufrufbar. Die Präsentation der Waren in unserem Internetshop stellen kein rechtlich bindendes Vertragsangebot unsererseits dar, sondern sind nur eine unverbindliche Aufforderungen an den Nutzer, Waren zu bestellen. Mit der Bestellung der gewünschten Ware gibt der Nutzer ein für ihn verbindliches Angebot auf Abschluss eines Kaufvertrages ab.
+        </p>
+        <p>
+            Die Annahme des Angebots erfolgt durch das ändern des Bestellstatuses durch den Betreiber und wird dem Nutzer in der Bestellübersicht angezeigt. Die Bestellübersicht ist über den Menüpunkt "Funktionen" aufrufbar. Bis das Angebot angenommen wurde kann der Nutzer das Angebot in der Bestellübersicht stornieren. Wenn das Angebot vom Betreiber angenommen wurde ist der Kaufvertrag zustande gekommen. 
+        </p>
+        <p>
+            Bei Eingang einer Bestellung in unserem Internetshop gelten folgende Regelungen: Der Nutzer gibt ein bindendes Vertragsangebot ab, indem er die in unserem Internetshop vorgesehene Bestellprozedur erfolgreich durchläuft.
+        </p>
+        <p>
+            Die Bestellung erfolgt in folgenden Schritten:
+        </p>
+        <ul>
+            <li>Auswahl der gewünschten Ware</li>
+            <li>Klicken auf den Knopf "Zur Kasse"</li>
+            <li>Prüfung bzw. Berichtigung der der Angaben im Warenkorb</li>
+            <li>Eingabe der Rechnungs- und Versandadresse</li>
+            <li>Wahl des Versandart (aktuell nur Standard durch die Deutsche Post)</li>
+            <li>Wahl der Zahlungsart (aktuell nur Rechnung)</li>
+            <li>Nochmalige Prüfung der eingegebenen Daten</li>
+            <li>Verbindliche Absendung der Bestellung durch Anklicken des Buttons "Kostenpflichtig bestellen"</li>
+        </ul>
+        <p>
+            Der Nutzer kann vor dem verbindlichen Absenden der Bestellung durch Betätigen der in dem von ihm verwendeten Internet-Browser enthaltenen "Zurück"-Taste nach Kontrolle seiner Angaben wieder zu der Internetseite gelangen, auf der die Angaben des Kunden erfasst werden und Eingabefehler berichtigen bzw. durch Schließen des Internetbrowsers den Bestellvorgang abbrechen.
+        </p>
+        <h5>Preise, Versandkosten, Zahlung, Fälligkeit</h5>
+        <p>
+            Die angegebenen Preise enthalten die gesetzliche Umsatzsteuer und sonstige Preisbestandteile. Hinzu kommen etwaige Versandkosten.
+        </p>
+        <p>
+            Alle Waren werden auf Rechnung verkauft.
+        </p>
+        <h5>Lieferung</h5>
+        <p>
+            Sofern wir dies in der Produktbeschreibung nicht deutlich anders angegeben haben, sind alle von uns angebotenen Artikel sofort versandfertig. Die Lieferung erfolgt hier spätesten innerhalb von 10 Werktagen.
+        </p>
+        <h5>Widerruf</h5>
+        <p>
+            Ein Widerruf ist möglich innerhalb von 14 Tagen nachdem der Kaufvertrag zu Stande gekommen ist und bedarf der Schriftform an die im Impressum angegebene Adresse, bevorzugt als E-Mail.
+        </p>
+        <p>
+            Im Falle eines Widerrufs müssen vom Nutzer alle Waren unverzüglich und spätestens binnen vierzehn Tagen an den Betreiber an die im Impressum angegebene Adresse zurückgesendet werden. Wurden die Waren zum Zeitpunkt des Widerrufs bereits versandt, werden die Versandkosten nicht zurückerstattet und der Nutzer hat ebenfalls die Kosten der Rücksendung zu tragen. Der Verkaufspreis der bestellten Waren wird dem Nutzer zurückgezahlt, sobald diese beim Betreiber eingetroffen sind, abzüglich eventueller Wertminderung durch Schäden an der Ware, die durch den Nutzer verursacht worden sind. Sind Schäden an der Ware durch Dritte entstanden, muss der Nutzer, wenn vom Betreiber gefordert, dazu beitragen Schadensersatzforderungen gegenüber dem Verursacher des Schadens geltend zu machen.
+        </p>
+        <h5>Gewährleistung</h5>
+        <p>
+            Es gelten die gesetzlichen Gewährleistungsregelungen.
+        </p>
+        <h5>Vertragssprache</h5>
+        <p>
+            Als Vertragssprache stehen Deutsch, Englisch und Dänisch zur Verfügung
+        </p>
+        <p>
+            <small>Diese AGB enthält ganze und teilweise abgewandelte Textbausteine der <a href="http://www.agb.de">Gratis AGB von agb.de</a></small>
+        </p>
+    </>);
+
+}
---
+++ mensch-id-frontend/src/localComponents/components/TermsAndConditions/TermsAndConditions_EN.tsx
@@ -0,0 +1,13 @@
+interface TermsAndConditions_ENProps {}
+
+export const TermsAndConditionsEN = (props: TermsAndConditions_ENProps) => {
+
+    return (
+    <>
+        <h1>Terms and Conditions</h1>
+        <p>
+            Please only create one 웃ID for yourself.
+        </p>
+    </>);
+
+}
